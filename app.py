e02dcf331bf1616ece8bff4179b62c61d182406e
mamola21 <105020968+mamola21@users.noreply.github.com>
2022-05-12 16:25:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc 
+import plotly.graph objs as go
 import pandas as pd
 
-app = Dash(__name__)
-app.layout = html.Div([html.H1('Ingenieria de sistemas'),
-                       html.Div('¿Qué es?')
-                       html.Div('Es la rama de la ingeniería relacionada con el manejo de las tecnologías y los sistemas de información.')
-                       html.Div('Esta contempla las bases teóricas y metodológicas necesarias para el diseño, la implantación, el análisis,')
-                       html.Div('el control, el procesamiento,el transporte, la operatividad, la toma de decisiones y la búsqueda de seguridad de los')
-                       html.Div('sistemas informáticos')
-                       
-app.layout = serve_layout
-if __name__ == '__main__':
-app.run_server(host='0.0.0.0',port=80)
+app = Dash(_name_)
+
+def serve_layout():
+df = pd.read_excel('portalwiki.xlsx')
+return html.Div([html.H1(df['Titulo']),
+                 html.Div(df['Tema1']),
+                 html.Div(df['Tema2'])])
+                 
+
+app.layout =serve_layout
+
+if_name== 'main_':
+
+app.run_server(host='0,0,0,0',port=80)
