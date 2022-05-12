09d900cebf2f3be035c2f55eba9da2ba9645c246
mamola21 <105020968+mamola21@users.noreply.github.com>
2022-05-12 17:34:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask
-from dash import Dash, html, dcc 
-import plotly.graph objs as go
+from dash import Dash, html, dcc
 import pandas as pd
+import plotly.graph_objs as go
 
+# Declarar objetos principales
 app = Dash(_name_)
 
+# Cargar la base de datos
 def serve_layout():
-df = pd.read_excel('portalwiki.xlsx')
-return html.Div([html.H1(df['Titulo']),
-                 html.Div(df['Tema1']),
-                 html.Div(df['Tema2'])])
-                 
+  df = pd.read_excel('portalwiki.xlsx')
+  return html.Div([html.H1(df['Titulo']),
+                       html.Div(df['Tema1']),
+                       html.Div(df['Tema2'])])                
+#funcion principal
+app.layout = serve_layout
 
-app.layout =serve_layout
-
-if_name== 'main_':
-
-app.run_server(host='0,0,0,0',port=80)
+if _name_ == '_main_':
+  #Cargar el objeto principal a todas las interfaces de red en el puerto 80
+  app.run_server(host='0.0.0.0',port=80)
